605522f54542d3d30fa48bbf2afbfa38ef6f0ba1
RadW2020 <RadW2020@users.noreply.github.com>
2016-04-21 22:07:26 +0200

    FormularioProfesor 0.1
    primera versión e inicio del repositorio
    
    Claudia y Raúl

---
+++ FormularioProfesor.java
@@ -0,0 +1,93 @@
+package gestionxml;
+
+/**
+ *
+ * @author Claudia y Raúl
+ */
+public class FormularioProfesor {
+
+    String profesor;
+    String dni;
+    String nombre;
+    String apellidos;
+    String nota;
+    String[] preferencias = new String[3];
+
+    public FormularioProfesor(  String profesor, String dni, String nombre, 
+                                String apellidos, String nota) {
+        this.profesor = profesor;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.nota = nota;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
